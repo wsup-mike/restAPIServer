70f37f767cd3ed7acc5d1f91d7278b067bfe000f
Mike Ramos <michaelangeloram8@gmail.com>
2024-02-18 19:22:20 -0600

    new commits

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const { validationResult } = require("express-validator");
 
 const Post = require("../models/post");
@@ -95,5 +98,105 @@ exports.getPost = (req, res, next) => {
 // To Edit an existing post (We will create shortly!)
 // Added as workaround to issue loading images
 exports.updatePost = (req, res, next) => {
-  const imageUrl = req.file.path.replace("\\", "/");
+  const postId = req.params.postId; // To extract the postId from the req params
+
+  // first validate the inputs for the new post
+   const errors = validationResult(req);
+   if (!errors.isEmpty()) {
+     const error = new Error(
+       "Data validation error! Entered data does NOT meet the requirements!"
+     );
+     error.statusCode = 422;
+     throw error;
+   }
+
+  
+  const title = req.body.title; // To extract the the title from req body
+  const content = req.body.content; // to extract the content from body
+  let imageUrl = req.body.image; 
+
+  if (req.file) {
+    imageUrl = req.file.path.replace("\\", "/"); // From the workaround
+  }
+
+  if (!imageUrl) { // a validation
+    const error = new Error("No image file chosen.");
+    error.statusCode = 422;
+    throw error;
+  }
+  
+  // Now to update the database using the Post model and Mongoose' findById()
+  Post
+    .findById(postId)
+    .then(post => { 
+      if (!post) { // First check if a existing post found
+        const error = new Error('Cant find the existing post');
+        error.statusCode = 500;
+        throw error;
+      }
+      // ..if post is found in database
+      // 1st check imageUrl of the prior existing post (To use new helper)
+      if (imageUrl !== post.imageUrl) { // meaning it changed...
+        clearImage(post.imageUrl);
+      }
+      post.title = title;
+      post.imageUrl = imageUrl;
+      post.content = content;
+      return post.save();
+    })
+    .then(result => {
+      res.status(200).json({
+        message: 'Post updated!',
+        post: result
+      });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
+
+exports.deletePost = ((req, res, next) => {
+  const postId = req.params.postId;
+  Post
+    .findById(postId)
+    .then(post => {
+      // 1st check if post exists in database
+      if (!post) {
+        const error = new Error('Could not find post.');
+        error.statusCode = 400;
+        throw error;
+      }
+      // Verify identity of logged in user 
+      
+      // Clear out the post's image stored
+      clearImage(post.imageUrl);
+
+      // To formally delete post from database
+      return Post.findByIdAndDelete(postId);
+
+    })
+    .then(result => {
+      console.log(result);
+      res.status(200).json({
+        message: 'Post has sucessfully been deleted!'
+      });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+
+
+});
+
+
+const clearImage = (filePath) => {
+  filePath = path.join(__dirname, '..', filePath);
+  fs.unlink(filePath, err => console.log(err));
+}
+
--- routes/feed.js
+++ routes/feed.js
@@ -22,4 +22,12 @@ router.post(
 // GET /feed/post
 router.get("/post/:postId", feedController.getPost);
 
+// POST /feed/post
+router.put("/post/:postId", [
+  body("title").trim().isLength({ min: 5 }),
+  body("content").trim().isLength({ min: 5 }),
+], feedController.updatePost);
+
+router.delete('/post/:postId', feedController.deletePost)
+
 module.exports = router;
